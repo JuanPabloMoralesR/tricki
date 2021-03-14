5ae3b95eba2488f5820cf0d4b3f01b6eff5f0553
Juan Pablo Morales Rincon <jupmoralesri@unal.edu.co>
2021-03-14 10:19:37 -0500

    Algoritmo de selección de ganador sin tener directamente las winning combinations quemadas

--- script.js
+++ script.js
@@ -12,9 +12,14 @@ const
 var userIsPlayingAs = "X";
 var userIcon = playerXIcon; 
 var botIcon = playerOIcon;
+var userTakenSpots = [];
+
+
+searchForWinningCombinatios([ 1, 7, 8, 9 ]);
 
 allBox.forEach(item => {
     item.addEventListener('click', event => {
+        userTakenSpots.push(parseInt(item.className));
         clickedBox(item)
     })
 })
@@ -37,6 +42,12 @@ selectOBtn.onclick = () =>{
 async function clickedBox(element){
     playBoard.style.pointerEvents = "none";
     addIconToSpan(element);
+    userTakenSpots.sort();
+    if(userTakenSpots.length >= 3){
+        if(searchForWinningCombinatios(userTakenSpots))
+            console.log("El jugador: " + userIsPlayingAs + " Ha ganado.");
+    }
+    console.log(userTakenSpots);
     let randomDelayTime = ((Math.random() * 1000) + 200).toFixed();
     await wait(randomDelayTime)
     bot();
@@ -55,7 +66,6 @@ function bot(){
         if(item.childElementCount == 0)
             freeSpots.push(index);
     })
-    console.log(freeSpots)
     let randomBox = freeSpots[Math.floor(Math.random() * freeSpots.length)];
     if(freeSpots.length > 0){
         addIconToSpan(allBox[randomBox], true);
@@ -72,4 +82,40 @@ function addIconToSpan(element, isBot = false){
     }
     element.innerHTML = icon;
     element.style.pointerEvents = "none"; // Una vez que el usuario seleccione un casilla no puede ser selecionada de nuevo
+}
+
+function searchForWinningCombinatios(array){ 
+    if(array.length === 3){
+        return isWinningCombination(array);
+    }
+
+    for (let index = 0; index < 4; index++) {
+        let tempArray = [...array] // En js, los arreglos son valores de referencia, por lo cual si se usa =, ambas variables estarán apuntando al mismo objeto
+        tempArray.splice(index, 1);
+        if(isWinningCombination(tempArray)){ 
+            return true;
+        }
+    }
+
+    return false;
+}
+
+function isWinningCombination(spots){
+    return consecutive(spots) || consecutive(spots, 3) || consecutive(spots, 4) || isDiagonal(spots);
+}
+
+function consecutive(array, difference = 1) {
+    var i = 2, d;
+    while (i < array.length) {
+        d = (array[i - 1]) - array[i - 2];
+        if (Math.abs(d) === difference && d === array[i] - array[i - 1]) {
+            return true;
+        }
+        i++;
+    }
+    return false;
+}
+
+function isDiagonal(array){ 
+    return array.reduce((a,b) => a + b, 0) == 15;
 }
